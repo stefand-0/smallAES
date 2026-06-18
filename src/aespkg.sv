a70e93b052bfbc8f50e8ce02c421b0c789329f9c
stefand-0 <stefandragan95@gmail.com>
2026-06-18 11:58:09 +0300

    Update aespkg.sv

--- src/aespkg.sv
+++ src/aespkg.sv
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
+// hex madness.
 package aes_pkg;
 
     localparam logic [7:0] SBOX [0:255] = '{
